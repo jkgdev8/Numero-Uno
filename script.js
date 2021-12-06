d54c9535b29ac39c0891e779cba1baf22e5d0154
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2021-12-05 18:31:14 -0800

    updated scores and search function

--- script.js
+++ script.js
@@ -14,18 +14,38 @@ var clickMe = function() {
         const goal = data.matches.score;
         const reverse = data.matches.length;
     
-            for (var i = 0; i <= 5; i++) {  
+            for (var i = 0; i <= 20; i++) {  
                 var date =  document.createElement("ul");
                 date.textContent = "Date:   " + data.matches[i].date + " " + data.matches[i].team1 + " vs " 
-                + data.matches[i].team2 + " Score: " + "  " +  data.matches[i].score.ft;
+                + data.matches[i].team2 + " Score: " + "  "  + data.matches[i].score.ft;
                 scores.appendChild(date);
             }
-
-            
+  
        
         })
 
     
+}
+
+function remove(me) {
+    document.getElementById(me).outerHTML = ""
+};
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
 
 
